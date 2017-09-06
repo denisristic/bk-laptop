a3cecff2feb82bc9767c360b17ebd2bb5f7acfd1
Valentino Bubalo (vbubalo) <vbubalo@tvz.hr>
2017-09-06 17:27:55 +0200

    loading user not solved

--- src/AppBundle/Controller/RegistrationController.php
+++ src/AppBundle/Controller/RegistrationController.php
@@ -1,25 +1,12 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: vbubalo
- * Date: 4.9.2017.
- * Time: 15:50
- */
-
 namespace AppBundle\Controller;
 
 use AppBundle\Form\UserType;
 use AppBundle\Entity\User;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\HttpFoundation\Response;
 
 class RegistrationController extends Controller
 {
@@ -28,27 +15,13 @@ class RegistrationController extends Controller
      */
     public function registerAction(Request $request, UserPasswordEncoderInterface $passwordEncoder)
     {
+        // 1) build the form
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
 
-        $form = $this->createFormBuilder()
-            ->add('username', TextType::class, array('attr' => array('class' => 'form-control', 'placeholder' => 'Enter your username')))
-            ->add('email', EmailType::class, array('attr' => array('class' => 'form-control', 'placeholder' => 'Enter your email')))
-            ->add('password', PasswordType::class, array('attr' => array('rows' => '10', 'class' => 'form-control', 'placeholder' => 'Enter your password here')))
-            ->add('plainPassword', PasswordType::class, array('attr' => array('rows' => '10', 'class' => 'form-control', 'placeholder' => 'Repeat your password here')))
-            ->add('submit', SubmitType::class, array('label' => 'Register!', 'attr' => array('class' => 'btn btn-lg btn-danger')))
-            ->getForm();
-
-
-
-
-
+        // 2) handle the submit (will only happen on POST)
+        $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-
-            $user = new User();
-            $form->handleRequest($request);
-
-            $formData = $form->getData();
-
-            $form = $this->createForm(UserType::class, $user);
 
             // 3) Encode the password (you could also do this via Doctrine listener)
             $password = $passwordEncoder->encodePassword($user, $user->getPlainPassword());
